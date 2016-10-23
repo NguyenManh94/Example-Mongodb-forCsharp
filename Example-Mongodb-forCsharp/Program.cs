ccd5b68cf72d4110279b2d1a0331ada1c3ba5116
ManhNguyen <nguyenmanhit.mak@gmail.com>
2016-10-23 17:57:58 +0700

    Count collection db

--- Example-Mongodb-forCsharp/Program.cs
+++ Example-Mongodb-forCsharp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,24 @@ namespace Example_Mongodb_forCsharp
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            var exampleDb = GetDatabase("example");
+
+            var collection = exampleDb.GetCollection<BsonDocument>("teacher");
+            var count = collection.Count(new BsonDocument());
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+
+        public static IMongoDatabase GetDatabase(string _dbName)
+        {
+            //Create object connect mongodb server
+            //var client = new MongoClient();
+
+            //Connect use a connect string
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            return client.GetDatabase(_dbName);
         }
     }
 }
